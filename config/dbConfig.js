d8206064d4e8cbc2c38fbd78a1b5c1db9c77e813
Nishant Kumar <nishantkumar222555@gmail.com>
2023-07-18 11:07:37 +0530

    hello

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.mongo_url);
+mongoose.connect(process.env.mongo_url)
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 
 connection.on("connected", () => {
   console.log("Mongo DB Connection Successfull");

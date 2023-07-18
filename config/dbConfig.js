7b4e47c2883c1b20796ce2850390101f03936aab
Nishant Kumar <nishantkumar222555@gmail.com>
2023-07-18 11:14:29 +0530

    hello

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,28 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.mongo_url)
+
+// const connection = mongoose.connection
+
+// connection.on("connected", () => {
+//   console.log("Mongo DB Connection Successfull");
+// });
+
+// connection.on("error", () => {
+//   console.log("Mongo DB Connection failed");
+// });
+
+// module.exports = mongoose;
+
 const mongoose = require("mongoose");
+const colors = require("colors");
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
 
-mongoose.connect(process.env.mongo_url)
-
-const connection = mongoose.connection
-
-connection.on("connected", () => {
-  console.log("Mongo DB Connection Successfull");
-});
-
-connection.on("error", () => {
-  console.log("Mongo DB Connection failed");
-});
-
-module.exports = mongoose;
+module.exports = connectDb;

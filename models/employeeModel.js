ed14ba3854ba47cd0c91439d2510937954db92a2
Nishant Kumar <nishantkumar222555@gmail.com>
2023-07-18 10:25:56 +0530

    first-commit

---
+++ models/employeeModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+  employeeId: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("employees", employeeSchema);
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const employeeSchema = new mongoose.Schema({
+//     employeeId : {
+//         type : String,
+//         required: true
+//     },
+//     password : {
+//         type : String,
+//         required: true
+//     },
+//     name : {
+//         type: String,
+//         required: true
+//     },
+//     approved: {
+//         type: Boolean,
+//         default : false
+//     }
+// });
+
+// module.exports = mongoose.model('employees',employeeSchema);
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const employeeRoute = require("./routes/employeeRoute");
+const studentRoute = require("./routes/studentRoute");
+const resultsRoute = require("./routes/resultsRoute");
+
+app.use("/api/employee/", employeeRoute);
+app.use("/api/student/", studentRoute);
+app.use("/api/results/", resultsRoute);
+
+
+
+const path = require("path");
+const port = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*' , (req,res) => {
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+app.listen(port, () => console.log(`Node JS app listening on port ${port}!`));
+
